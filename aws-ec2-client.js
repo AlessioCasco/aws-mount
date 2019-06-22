b97a04e9e6c6ba2ffcfb717d9e1ccf5710e6f7b8
Jurgo Boemo <jurgo.boemo@gmail.com>
2019-06-22 19:09:51 +0100

    add region

--- aws-ec2-client.js
+++ aws-ec2-client.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk');
 const util = require('util');
+const {EC2_REGION} = process.env
 
-const ec2 = new AWS.EC2({apiVersion: '2016-11-15'})
+const ec2 = new AWS.EC2({apiVersion: '2016-11-15', region: EC2_REGION})
 
 function addAsyncMethods(obj, methods){
   const a = methods
