91ec3caaa45697e1865652b36df1571ef2bfe371
Alessio Casco <alessio@truelayer.com>
2019-06-23 01:10:28 +0100

    removed .filters

--- aws-ec2-client.js
+++ aws-ec2-client.js
@@ -6,7 +6,6 @@ const ec2 = new AWS.EC2({apiVersion: '2016-11-15', region: EC2_REGION})
 
 function addAsyncMethods(obj, methods){
   const a = methods
-    .filter((key) => typeof  obj[key] === 'function')
     .forEach(key => {
       obj[key+"Async"] = util.promisify(obj[key])
     })
