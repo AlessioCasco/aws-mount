b8c2effc5049cd10201e3f5c57b01464fc68ddc7
Alessio Casco <alessio@truelayer.com>
2019-06-23 01:11:13 +0100

    .

--- aws-ec2-client.js
+++ aws-ec2-client.js
@@ -5,7 +5,7 @@ const EC2_REGION = process.env.EC2_REGION
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15', region: EC2_REGION})
 
 function addAsyncMethods(obj, methods){
-  const a = methods
+  methods
     .forEach(key => {
       obj[key+"Async"] = util.promisify(obj[key])
     })
